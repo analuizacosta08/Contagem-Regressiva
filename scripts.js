326d0db4cfbb02056f9a78f91d05f3b65f20d3ce
Ana Luiza Costa <117484983+analuizacosta08@users.noreply.github.com>
2023-04-19 16:01:34 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-var countdownInterval; // Declarar countdownInterval fora da função startCountdown
+var countdownInterval; 
 
 function startCountdown() {
   var countdown = document.getElementById("countdown");
   var number = 10; // número inicial
   countdown.innerHTML = number;
 
-  countdownInterval = setInterval(function() { // Armazenar o intervalo em countdownInterval
+  countdownInterval = setInterval(function() { 
     number--;
     countdown.innerHTML = number;
 
@@ -16,7 +16,7 @@ function startCountdown() {
   }, 1000);
 }
 
-function stopCountdown() { // Criar a função stopCountdown
-  clearInterval(countdownInterval); // Limpar o intervalo de tempo armazenado em countdownInterval
-  alert("Contagem regressiva interrompida!"); // Exibir uma mensagem informando que a contagem regressiva foi interrompida
+function stopCountdown() { 
+  clearInterval(countdownInterval); 
+  alert("Contagem regressiva interrompida!"); 
 }
